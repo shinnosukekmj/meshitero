c479f57829f9e5c3d4421f0e60060545403fd084
EC2 Default User <ec2-user@ip-172-31-39-48.ec2.internal>
2022-01-07 05:10:01 +0000

    new3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   
   def get_profile_image(size)
     unless profile_image.attached?
-      file_path=Rails.rooy.join('app/assets/images/sample-author1.jpg')
+      file_path=Rails.root.join('app/assets/images/sample-author1.jpg')
       profile_image.attach(io: File.open(file_path),filename: 'default-image-png',content_type: 'image/png')
     end
     profile_image.variant(resize: size).processed
